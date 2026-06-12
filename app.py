7a2dd68070c682c2f3e98d783aaef79d6a1f6582
Nguyen Thanh Son <122671435+ThanhSon123E@users.noreply.github.com>
2026-06-12 13:19:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -933,4 +933,5 @@ def profile():
     return render_template("profile.html", active_page='profile')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
